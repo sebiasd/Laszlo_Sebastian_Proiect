1ec55ea99845d9f6f40378e568bc0295b15f9de7
sebiasd <laszlosebastian157@gmail.com@gmail.com>
2024-01-19 23:59:19 +0200

    upate

--- Pages/Tattoos/Index.cshtml.cs
+++ Pages/Tattoos/Index.cshtml.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Laszlo_Sebastian_Proiect.Data;
 using Laszlo_Sebastian_Proiect.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Laszlo_Sebastian_Proiect.Pages.Tattoos
 {
@@ -19,20 +18,30 @@ namespace Laszlo_Sebastian_Proiect.Pages.Tattoos
             _context = context;
         }
 
-        public IList<Tattoo> Tattoo { get;set; } = default!;
+        public IList<Tattoo> Tattoo { get; set; } = default!;
+        public IList<string> UniqueArtists { get; set; } = new List<string>();
+        public IList<Tattoo> FilteredTattoos { get; set; } = new List<Tattoo>();
+
+        [BindProperty(SupportsGet = true)]
+        public string SelectedArtist { get; set; } = "";
 
         public async Task OnGetAsync()
         {
-            if (_context.TattooArtist != null)
+            if (_context.Tattoo != null)
             {
                 Tattoo = await _context.Tattoo
-                .Include(r => r.Location)
-                .Include(r => r.TattooArtist)
-                .Include(r => r.Style)
+                    .Include(r => r.Location)
+                    .Include(r => r.TattooArtist)
+                    .Include(r => r.Style)
+                    .ToListAsync();
 
+                // Populate UniqueArtists for dropdown
+                UniqueArtists = Tattoo.Select(t => t.TattooArtist.ArtistName).Distinct().ToList();
 
-                .ToListAsync();
-
+                // Apply filtering based on the selected artist
+                FilteredTattoos = string.IsNullOrEmpty(SelectedArtist)
+                    ? Tattoo.ToList()
+                    : Tattoo.Where(t => t.TattooArtist.ArtistName == SelectedArtist).ToList();
             }
         }
     }
